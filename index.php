825610f119afdf9ac4e4d0cdd7f5ebe59c9b838c
Kiran94 <kiran_patel94@hotmail.com>
2015-06-10 21:30:43 +0100

    added template

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html> 
+<html lang='en'>
+<head>
+	<title>Placement Logger</title>
+
+	<meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <link rel="stylesheet" type="text/css" href="styles/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="styles/custom.css" />
+</head>
+<body>
+
+
+	<script type="text/javascript" src="scripts/jquery.min.js"></script>
+	<script type="text/javascript" src="scripts/bootstrap.min.js"></script>
+</body>
+</html>
