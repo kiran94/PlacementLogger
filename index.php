fbcb76bbaed5ff2573f93db3b8aacd55956eaa3c
Kiran94 <kiran_patel94@hotmail.com>
2015-06-10 21:37:07 +0100

    added container

--- index.php
+++ index.php
@@ -11,7 +11,23 @@
     <link rel="stylesheet" type="text/css" href="styles/custom.css" />
 </head>
 <body>
+	<!-- CONTAINER -->
+	<div class='container'>
+		<!-- ROW -->
+		<div class='row'>
+			<div class='col-xs-12'>
+				<h1>Placement Logger</h1>
+			</div>
+		</div>
+		<!-- END ROW -->
 
+		
+
+
+
+
+	</div>
+	<!-- END CONTAINER -->
 
 	<script type="text/javascript" src="scripts/jquery.min.js"></script>
 	<script type="text/javascript" src="scripts/bootstrap.min.js"></script>
