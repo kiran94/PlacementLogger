d9789b411a4527116b3d51dabb7f2d06357365bf
Kiran94 <kiran_patel94@hotmail.com>
2015-06-11 15:47:18 +0100

    added latest api script

---
+++ getLatest.php
@@ -0,0 +1,18 @@
+<?php
+	require_once "api/connection.php";
+
+	$connect = new connection(); 
+
+	$con = $connect->makeConnection(); 
+
+	$query = "SELECT log_date FROM log ORDER BY log_id DESC LIMIT 1";
+
+	$result = mysqli_query($con, $query);
+
+	while($row = mysqli_fetch_array($result))
+	{
+		echo $row['log_date'];
+	}
+
+	mysqli_close($con);
+?>
