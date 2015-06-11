1e626964a1704dd781a0e3dbd26f42777c10f125
Kiran94 <kiran_patel94@hotmail.com>
2015-06-11 16:26:48 +0100

    removed dollar sign

--- DailyChecker/main.py
+++ DailyChecker/main.py
@@ -16,7 +16,7 @@ while(True):
 	printTerminal("Making GET request..")
 	req = makeRequest()
 	url = "http://kiransprojects.co.uk/PlacementLogger/api/getLatest.php"
-	date = $req.request(url)
+	date = req.request(url)
 
 	#Check todays date
 	todays_date = time.strftime("%Y-%m-%d")
