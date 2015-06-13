1e14b0fd26129ba1eb29ac07b915637d3b6dbc21
Kiran94 <kiran_patel94@hotmail.com>
2015-06-13 10:51:53 +0100

    ammended month array

--- HistoryFormat.php
+++ HistoryFormat.php
@@ -3,7 +3,7 @@
 	{
 		public function getMonth($month_number)
 		{
-			$month_numbers = array("Jan", "Feb", "Mar", "April", "June", "July", "August", "September", "October", "November", "December");
+			$month_numbers = array("Jan", "Feb", "Mar", "April", "May", "June", "July", "August", "September", "October", "November", "December");
 
 			return $month_numbers[$month_number-1];
 		}
