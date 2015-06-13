63f10582edc3995c7c75d94a55fb540269fb3880
Kiran94 <kiran_patel94@hotmail.com>
2015-06-13 11:15:35 +0100

    added full names to month array

--- HistoryFormat.php
+++ HistoryFormat.php
@@ -3,7 +3,7 @@
 	{
 		public function getMonth($month_number)
 		{
-			$month_numbers = array("Jan", "Feb", "Mar", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+			$month_numbers = array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
 
 			return $month_numbers[$month_number-1];
 		}
